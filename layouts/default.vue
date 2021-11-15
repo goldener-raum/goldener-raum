735f6599f587ff68b701b5def8d05b89dc4ba9fa
nmauersberg <n.mauersberg@gmail.com>
2021-11-15 21:35:00 +0100

    Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <w-app>
+    <Nuxt />
+  </w-app>
+</template>
+
+<script>
+import '~/style/global-layout.css';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+export default {};
+</script>
+
+<style scoped></style>
