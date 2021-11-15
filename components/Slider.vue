6483566add9a62f2379427fe25bfa512716ce718
nmauersberg <n.mauersberg@gmail.com>
2021-11-15 21:34:05 +0100

    Implement slider component

---
+++ components/Slider.vue
@@ -0,0 +1,42 @@
+<template>
+  <VueperSlides class="ma8 grow">
+    <VueperSlide v-for="(slide, i) in slides" :key="i" :image="slide.image">
+      <template #content>
+        <h2 class="text-right mt3 mr6">{{ slide.title }}</h2>
+        <h4 class="text-right mt3 mr6">{{ slide.content }}</h4>
+      </template>
+    </VueperSlide>
+  </VueperSlides>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides';
+import 'vueperslides/dist/vueperslides.css';
+
+export default {
+  components: { VueperSlides, VueperSlide },
+  data() {
+    return {
+      slides: [
+        {
+          title: 'Goldener Raum Bild 1',
+          content: 'Beschreibung',
+          image: require('@/assets/slider/goldener-raum-01.png'),
+        },
+        {
+          title: 'Goldener Raum Bild 2',
+          content: 'Beschreibung',
+          image: require('@/assets/slider/goldener-raum-02.png'),
+        },
+        {
+          title: 'Goldener Raum Bild 3',
+          content: 'Beschreibung',
+          image: require('@/assets/slider/goldener-raum-03.png'),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
