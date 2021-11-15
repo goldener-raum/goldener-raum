327cdeea33c042db04891f0f03c829282cfebb3f
nmauersberg <n.mauersberg@gmail.com>
2021-11-15 21:36:07 +0100

    Implement components as single page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="headline ma8 mb12" style="font-size: 4rem">
+      Betritt den goldenen Raum
+    </h2>
+
+    <Introduction />
+
+    <h2 class="title1 ml8 mt8">Trag dich ein!</h2>
+    <w-flex class="basis-zero">
+      <NewSubscriber />
+      <ListSubscribers />
+    </w-flex>
+
+    <h2 class="title1 ml8 mt8">Unsere Projekte</h2>
+    <Slider />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
