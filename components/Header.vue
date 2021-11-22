7f997724d402ab3d4a376f79a6bd76c6a0105d0e
nmauersberg <n.mauersberg@gmail.com>
2021-11-22 19:08:27 +0100

    Implement header

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="sh1" id="pageHeaderDefault">
+    <w-flex style="padding: 1.1rem">
+      <p class="mx4">Der&nbsp;goldene&nbsp;Raum</p>
+      <div class="grow" />
+      <p class="mx4">Über&nbsp;uns</p>
+      <p class="mx4">Unterstützen</p>
+    </w-flex>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+#pageHeaderDefault {
+  border-bottom: 1px solid lightgrey;
+  padding: 0.5rem;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
-  <w-app>
-    <Nuxt />
+  <w-app id="app">
+    <Header id="stickyHeader" />
+    <main class="grow">
+      <Nuxt />
+    </main>
   </w-app>
 </template>
 
@@ -11,4 +14,13 @@ import '@mdi/font/css/materialdesignicons.min.css';
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+#stickyHeader {
+  background: white;
+  height: 3.5rem;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 20;
+}
+</style>
