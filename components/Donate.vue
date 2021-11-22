ae0c9ae876607cf114ccd12889170e8d7d5884d9
nmauersberg <n.mauersberg@gmail.com>
2021-11-22 19:07:48 +0100

    Add donate and video section

---
+++ components/Donate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="margin: 5rem 4rem">
+    <w-flex basis-zero>
+      <div class="grow">
+        <h2 class="headline mt8 mb4 mr8 white" style="font-size: 3rem">
+          Werde Teil des goldenen Raumes.
+        </h2>
+        <p class="white py4 title2 grow">
+          Kurzer Erklärtext: Richte hier eine Dauerspende ein und trage dich
+          unten ins goldene Buch ein, um deine Unterstützung auch öffentlich zu
+          zeigen. Was bedeutet es? Warum 1 Promille?
+        </p>
+      </div>
+      <div
+        v-if="!$waveui.breakpoint.xs && !$waveui.breakpoint.sm"
+        class="grow"
+      />
+    </w-flex>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ components/Video.vue
@@ -0,0 +1,91 @@
+<template>
+  <div
+    :style="`margin: ${$waveui.breakpoint.width / 500}em ${
+      $waveui.breakpoint.width / 150
+    }em`"
+  >
+    <div class="placeholderWrapper">
+      <div
+        class="placeholderVideo"
+        v-if="!showVideo && !showVideoLock"
+        @mouseover="showVideo = true"
+        @mouseleave="showVideo = false"
+        @click="showVideo = true"
+        :style="`padding: ${$waveui.breakpoint.width / 400}em ${
+          $waveui.breakpoint.width / 300
+        }em`"
+      >
+        <w-flex column>
+          <h2
+            class="headline mb4 ml4 grow"
+            :style="`font-size: ${$waveui.breakpoint.width / 20}px;`"
+          >
+            Betritt den <br />
+            goldenen Raum
+          </h2>
+          <w-icon
+            color="primary"
+            :style="`font-size: ${$waveui.breakpoint.width / 7.5}px;`"
+          >
+            mdi mdi-arrow-right-drop-circle-outline
+          </w-icon>
+        </w-flex>
+      </div>
+      <div
+        class="placeholderVideo"
+        @mouseleave="showVideo = false"
+        @click="showVideoLock = !showVideoLock"
+        v-else
+      >
+        <w-flex style="height: 100%" justify-center align-center>
+          <w-icon
+            v-if="!showVideoLock"
+            color="secondary"
+            :style="`font-size: ${$waveui.breakpoint.width / 7.5}px`"
+          >
+            mdi mdi-arrow-right-drop-circle-outline
+          </w-icon>
+          <w-icon
+            v-else
+            color="secondary"
+            :style="`font-size: ${$waveui.breakpoint.width / 7.5}px`"
+            spin
+          >
+            mdi mdi-motion-pause-outline
+          </w-icon>
+        </w-flex>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showVideo: false,
+      showVideoLock: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.placeholder {
+  margin: 2rem 8rem;
+}
+.placeholderWrapper {
+  border: 1px solid gray;
+  position: relative;
+  padding-bottom: 56.25%;
+}
+.placeholderVideo {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: white;
+  cursor: pointer;
+}
+</style>
