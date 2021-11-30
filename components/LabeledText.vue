79bc0e820147695c54abf6e93c8a52312881f2ea
nmauersberg <n.mauersberg@gmail.com>
2021-11-30 13:56:51 +0100

    Add displayable bank details to twingle

---
+++ components/LabeledText.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb3">
+    <p class="caption disableSelect">{{ label }}:</p>
+    <p>
+      {{ content }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    content: String,
+  },
+};
+</script>
+
+<style scoped>
+.disableSelect {
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+}
+</style>
--- components/Twingle.vue
+++ components/Twingle.vue
@@ -14,10 +14,38 @@
     />
     <script src="https://spenden.twingle.de/embed/generic"></script>
     <!-- twingle -->
+    <w-divider class="mx6">ODER</w-divider>
+    <w-flex class="justify-space-around" v-if="!showBankDetails">
+      <w-button
+        @click="showBankDetails = true"
+        bg-color="secondary"
+        color="white"
+        class="pa6 my6"
+        lg
+      >
+        Bankverbindung anzeigen
+      </w-button>
+    </w-flex>
+    <div class="pa8" v-else>
+      <h2 class="mb6">Bankverbindung für deine Unterstützung:</h2>
+      <LabeledText :label="'Zahlungsempfänger'" :content="'Goldener Raum'" />
+      <LabeledText :label="'IBAN'" :content="'DE48 4306 0967 7918 8877 00'" />
+      <LabeledText :label="'BIC'" :content="'GENODEM1GLS'" />
+      <LabeledText
+        :label="'Verwendungszweck'"
+        :content="'Goldener Raum (2183SN8)'"
+      />
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showBankDetails: false,
+    };
+  },
+};
 </script>
 
 <style scoped></style>
