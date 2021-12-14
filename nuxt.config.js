2f280513be51a9074267bc2ecd2509ded5c00a0c
nmauersberg <n.mauersberg@gmail.com>
2021-12-14 10:17:40 +0100

    Add meta tags: title, description and image

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,38 @@
+const descriptionMeta =
+  'Die Welt zu einem schöneren und nachhaltigeren Ort machen – dafür unterstützen sich Menschen gegenseitig hier im goldenen Raum.';
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'goldener-raum',
+    title: 'Goldener Raum',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'de',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'og:title', name: 'og:title', content: 'Goldener Raum' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: descriptionMeta,
+      },
+      {
+        hid: 'og:description',
+        name: 'og:description',
+        content: descriptionMeta,
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'https://www.goldenerraum.de/meta/preview.png',
+      },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hide: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
